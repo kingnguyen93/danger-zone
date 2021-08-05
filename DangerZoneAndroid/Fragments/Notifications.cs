9fe82b2307e8cbc77148e53dc30365ca62f68b90
King Nguyen <socolatuithan@gmail.com>
2021-08-06 06:45:10 +0700

    Add project files.

---
+++ DangerZoneAndroid/Fragments/Notifications.cs
@@ -0,0 +1,38 @@
+﻿using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using AndroidX.Fragment.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DangerZoneAndroid.Fragments
+{
+    public class Notifications : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public static Notifications NewInstance()
+        {
+            var frag = new Notifications { Arguments = new Bundle() };
+            return frag;
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var ignored = base.OnCreateView(inflater, container, savedInstanceState);
+
+            // Use this to return your custom view for this Fragment
+            return inflater.Inflate(Resource.Layout.notifications, container, false);
+        }
+    }
+}
